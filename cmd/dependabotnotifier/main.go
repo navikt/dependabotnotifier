998adaff19cc84091a6498f1c32594385a38d075
J-K. Solbakken <jksolbakken@gmail.com>
2023-07-11 14:19:38 +0200

    reenable all the things

--- cmd/dependabotnotifier/main.go
+++ cmd/dependabotnotifier/main.go
@@ -1,33 +1,36 @@
 package main
 
 import (
+	"dependabotshamer/pkg/github"
 	"dependabotshamer/pkg/slack"
+	"dependabotshamer/pkg/teams"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	//githubToken := envOrDie("GITHUB_TOKEN")
-	//teamsToken := envOrDie("TEAMS_TOKEN")
+	githubToken := envOrDie("GITHUB_TOKEN")
+	teamsToken := envOrDie("TEAMS_TOKEN")
 	slackToken := envOrDie("SLACK_TOKEN")
 
-	//reposWithoutDepandabotAlerts, err := github.ReposWithDependabotAlertsDisabled("navikt", githubToken)
-	//if err != nil {
-	//	log.Fatalf("unable to retrieve repos: %v", err)
-	//}
-	//fmt.Printf("Found %d repos with Depenadbot disabled", len(reposWithoutDepandabotAlerts))
-	//
-	//repoOwners := make(map[string][]teams.Team)
-	//for _, repo := range reposWithoutDepandabotAlerts {
-	//	repoFullName := fmt.Sprintf("%s/%s", repo.Owner.Name, repo.Name)
-	//	teamsWithAdmin, err := teams.AdminsFor(repoFullName, teamsToken)
-	//	if err != nil {
-	//		fmt.Printf("%v", err)
-	//		os.Exit(1)
-	//	}
-	//	repoOwners[repoFullName] = teamsWithAdmin
-	//}
-	//fmt.Printf("%v", repoOwners)
+	reposWithoutDepandabotAlerts, err := github.ReposWithDependabotAlertsDisabled("navikt", githubToken)
+	if err != nil {
+		log.Fatalf("unable to retrieve repos: %v", err)
+	}
+	fmt.Printf("Found %d repos with Depenadbot disabled", len(reposWithoutDepandabotAlerts))
+
+	repoOwners := make(map[string][]teams.Team)
+	for _, repo := range reposWithoutDepandabotAlerts {
+		repoFullName := fmt.Sprintf("%s/%s", repo.Owner.Name, repo.Name)
+		teamsWithAdmin, err := teams.AdminsFor(repoFullName, teamsToken)
+		if err != nil {
+			fmt.Printf("%v", err)
+			os.Exit(1)
+		}
+		repoOwners[repoFullName] = teamsWithAdmin
+	}
+	fmt.Printf("%v", repoOwners)
 
 	err := slack.SendMessage("C05G9377NRL", "Hei 👋\nDette er en melding over flere \nlinjer.", slackToken)
 	if err != nil {

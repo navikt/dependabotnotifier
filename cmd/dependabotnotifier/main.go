618670d2582c4f67d3a7e661aaa8e0d7b9183952
J-K. Solbakken <jksolbakken@gmail.com>
2023-07-13 12:32:39 +0200

    msg tweaking

--- cmd/dependabotnotifier/main.go
+++ cmd/dependabotnotifier/main.go
@@ -35,7 +35,7 @@ func main() {
 		for _, owner := range owners {
 			fmt.Printf("Notifying %s about %s in %s\n", owner.Slug, repo, owner.SlackChannel)
 			heading := fmt.Sprintf(`:wave: *Hei, %s* :github2:`, owner.Slug)
-			msg := fmt.Sprintf(`Du er admin i GitHub-repoet "%s". Dette repoet har ikke Dependabot alerts aktivert.\nDependabot hjelper deg å oppdage biblioteker med kjente sårbarheter i appene dine.\nDu kan sjekke status og enable Dependabot <https://github.com/%s/security|her>.`, repo, repo)
+			msg := fmt.Sprintf(`Dere er admins i GitHub-repoet *%s*. Dette repoet har ikke Dependabot alerts aktivert. Dependabot hjelper deg å oppdage biblioteker med kjente sårbarheter i appene dine. Du kan sjekke status og enable Dependabot <https://github.com/%s/security|her>. Hvis repoet ikke er i bruk, vurder å arkivere det. Det kan gjøres nederst på <https://github.com/%s/settings|denne siden>.`, repo, repo, repo)
 			err = slack.SendMessage("#jk-tullekanal", heading, msg, slackToken)
 		}
 	}
